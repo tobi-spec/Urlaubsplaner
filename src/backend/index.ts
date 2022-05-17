8015e8661f4863a997a8ff0372faef2cdfa49470
Tobias <post@tobias-weiland.eu>
2022-05-17 20:19:47 +0200

    basic controller

--- src/backend/index.ts
+++ src/backend/index.ts
@@ -1,1 +1,11 @@
-console.log('Hi');
+const express = require("express");
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+  res.send('Hello beautiful World!');
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
