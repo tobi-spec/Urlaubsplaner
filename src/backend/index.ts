1cbccb925d30aebb4c01587fd403dc3c3bd12631
Tobias <post@tobias-weiland.eu>
2022-05-19 20:34:55 +0200

    create basic request for employees

--- src/backend/index.ts
+++ src/backend/index.ts
@@ -2,8 +2,13 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => {
-  res.send('Hello beautiful World!');
+app.get('/liveliness', (req, res) => {
+  res.send('Server Runs!');
+});
+
+app.get('/employees', (req, res) => {
+  const employees = ["Sven", "Jörg", "Martina", "Ali", "Christian", "Tami", "Ute"]
+  res.send(employees);
 });
 
 app.listen(port, () => {
