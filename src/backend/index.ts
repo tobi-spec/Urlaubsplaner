f762d80d01357555bffce514a8e94817f66aa554
Tobias <post@tobias-weiland.eu>
2022-05-31 11:01:14 +0200

    cet chart names by request

--- src/backend/index.ts
+++ src/backend/index.ts
@@ -2,12 +2,18 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "http://127.0.0.1:5500");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 app.get('/liveliness', (req, res) => {
   res.send('Server Runs!');
 });
 
 app.get('/employees', (req, res) => {
-  const employees = ["Sven", "Jörg", "Martina", "Ali", "Christian", "Tami", "Ute"]
+  const employees = ["Joachim", "Jörg", "Martina", "Ali", "Christian", "Tami", "Ute"]
   res.send(employees);
 });
 
