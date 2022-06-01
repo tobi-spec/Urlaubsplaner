000f52fc5b08be4e9a3e99f936b7e3bca9acd9f6
Tobias <post@tobias-weiland.eu>
2022-06-01 12:28:09 +0200

    refactor barColors into jsonfile

--- src/EmployeeController.ts
+++ src/EmployeeController.ts
@@ -8,88 +8,16 @@ app.get('/status', (req, res) => {
 });
 
 app.get('/data', (req, res) => {
-  const employees = adapter.getNames()
-  const employeeFirstVacationArray = [
-    ['2022-01-01', '2022-03-03'],
-    ['2022-04-03', '2022-04-23'],
-    ['2022-08-06', '2022-09-07'],
-    ['2022-03-07', '2022-04-09'],
-    ['2022-07-09', '2022-09-13'],
-    ['2022-10-13', '2022-10-31'],
-    ['2022-02-15', '2022-03-21']
-]
-
-  const employeeSecondVacationArray = [
-    ['2022-10-03', '2022-11-30'],
-    ['2022-11-03', '2022-11-23'],
-    ['2022-02-06', '2022-03-07'],
-    ['2022-07-07', '2022-08-09'],
-    ['2022-01-09', '2022-02-13'],
-    ['2022-02-13', '2022-03-31'],
-    ['2022-11-15', '2022-12-21']
-  ]
-
-  const chartColors = [
-  'rgba(255, 26, 104, 1)',
-    'rgba(54, 162, 235, 1)',
-    'rgba(255, 206, 86, 1)',
-    'rgba(75, 192, 192, 1)',
-    'rgba(153, 102, 255, 1)',
-    'rgba(255, 159, 64, 1)',
-    'rgba(0, 0, 0, 1)'
-  ]
-
-
-  // setup 
-  const data = {
-  labels: employees,
-  datasets: [{
-    data: employeeFirstVacationArray,
-    backgroundColor: chartColors
-  },
-  {
-    data: employeeSecondVacationArray,
-    backgroundColor: chartColors
-  }
-  ]
-  };
-
-// config 
-// request from backend
-const config = {
-type: 'bar',
-data,
-options: {
-  plugins: {
-    legend: {
-      display: false
-            }
-          },
-  indexAxis: 'y',
-  scales: {
-      xAxes: {
-          min: '2022-01-01',
-          max: '2022-12-31',
-          type: 'time',
-          time: {
-              unit: 'day'
-          }
-      },
-    yAxes:{
-    }
-  }
-}
-};
+  const config = adapter.getPlotData()
   res.send(config);
 });
 
-
 app.get('/index', function(req, res) {
-  res.sendFile('/home/tobi/projects/Urlaubsplaner/src/backend/index.html');
+  res.sendFile('/home/tobi/projects/Urlaubsplaner/src/index.html');
 });
 
 app.get('/index.css', function(req, res) {
-  res.sendFile('/home/tobi/projects/Urlaubsplaner/src/backend/index.css');
+  res.sendFile('/home/tobi/projects/Urlaubsplaner/src/index.css');
 });
 
 app.listen(port, () => {
--- src/EmployeeJSONAdapter.ts
+++ src/EmployeeJSONAdapter.ts
@@ -1,10 +1,79 @@
 const fs = require("fs")
 var jsonata = require("jsonata");
 
-export function getNames(): string[] {
+export function getPlotData() {
+    const employeeFirstVacationArray = [
+        ['2022-01-01', '2022-03-03'],
+        ['2022-04-03', '2022-04-23'],
+        ['2022-08-06', '2022-09-07'],
+        ['2022-03-07', '2022-04-09'],
+        ['2022-07-09', '2022-09-13'],
+        ['2022-10-13', '2022-10-31'],
+        ['2022-02-15', '2022-03-21']
+    ]
+
+    const employeeSecondVacationArray = [
+        ['2022-10-03', '2022-11-30'],
+        ['2022-11-03', '2022-11-23'],
+        ['2022-02-06', '2022-03-07'],
+        ['2022-07-07', '2022-08-09'],
+        ['2022-01-09', '2022-02-13'],
+        ['2022-02-13', '2022-03-31'],
+        ['2022-11-15', '2022-12-21']
+    ]
+
+    const data = {
+    labels: getNames(),
+    datasets: [{
+        data: employeeFirstVacationArray,
+        backgroundColor: getColors()
+    },
+    {
+        data: employeeSecondVacationArray,
+        backgroundColor: getColors()
+    }
+    ]
+    };
+
+    const config = {
+    type: 'bar',
+    data,
+    options: {
+    plugins: {
+        legend: {
+        display: false
+                }
+            },
+    indexAxis: 'y',
+    scales: {
+        xAxes: {
+            min: '2022-01-01',
+            max: '2022-12-31',
+            type: 'time',
+            time: {
+                unit: 'day'
+            }
+        },
+        yAxes:{
+        }
+            }
+        }
+    };
+    return config
+}
+
+
+function getNames(): string[] {
     const rawData = fs.readFileSync("./data.json", "utf-8");
     const data = JSON.parse(rawData);
     const expression = jsonata("employees.name")
+    return expression.evaluate(data)
+}
+
+function getColors(): string[] {
+    const rawData = fs.readFileSync("./colors.json", "utf-8");
+    const data = JSON.parse(rawData);
+    const expression = jsonata("colors")
     return expression.evaluate(data)
 }
 
