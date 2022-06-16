69582e79554f344843374e88b7bdf194cf9c964f
Tobias <post@tobias-weiland.eu>
2022-06-16 12:05:01 +0200

    add types to controller

--- src/EmployeeController.ts
+++ src/EmployeeController.ts
@@ -1,23 +1,23 @@
-import express from 'express';
+import express, { Request, Response } from 'express';
 const adapter = require("./EmployeeJSONAdapter")
 const app = express();
 const port = 3000;
 
-app.get('/status', (req, res) => {
+app.get('/status', (req: Request, res: Response) => {
   const result = adapter.createDataSets()
   res.send(result)
 });
 
-app.get('/data', (req, res) => {
+app.get('/data', (req: Request, res: Response) => {
   const config = adapter.createPlotConfig()
   res.send(config);
 });
 
-app.get('/index', function(req, res) {
+app.get('/index', function(req: Request, res: Response) {
   res.sendFile('./static/index.html', { root: __dirname });
 });
 
-app.get('/index.css', function(req, res) {
+app.get('/index.css', function(req: Request, res: Response) {
   res.sendFile('/static/index.css', { root: __dirname });
 });
 
