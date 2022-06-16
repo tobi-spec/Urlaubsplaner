9b4231e5fa62e0a8c95754fc1801674abf3bea7e
Tobias <post@tobias-weiland.eu>
2022-06-16 14:37:46 +0200

    set up prettier and run it

--- src/EmployeeController.ts
+++ src/EmployeeController.ts
@@ -1,23 +1,23 @@
-import express, { Request, Response } from 'express';
-import adapter = require("./EmployeeJSONAdapter")
+import express, { Request, Response } from "express";
+import adapter = require("./EmployeeJSONAdapter");
 const app = express();
 const port = 3000;
 
-app.get('/status', (req: Request, res: Response) => {
-  res.send("Server runs!")
+app.get("/status", (req: Request, res: Response) => {
+  res.send("Server runs!");
 });
 
-app.get('/data', (req: Request, res: Response) => {
-  const config = adapter.createPlotConfig()
+app.get("/data", (req: Request, res: Response) => {
+  const config = adapter.createPlotConfig();
   res.send(config);
 });
 
-app.get('/index', function(req: Request, res: Response) {
-  res.sendFile('./static/index.html', { root: __dirname });
+app.get("/index", function (req: Request, res: Response) {
+  res.sendFile("./static/index.html", { root: __dirname });
 });
 
-app.get('/index.css', function(req: Request, res: Response) {
-  res.sendFile('/static/index.css', { root: __dirname });
+app.get("/index.css", function (req: Request, res: Response) {
+  res.sendFile("/static/index.css", { root: __dirname });
 });
 
 app.listen(port, () => {
--- src/EmployeeJSONAdapter.ts
+++ src/EmployeeJSONAdapter.ts
@@ -2,82 +2,82 @@ import fs from "fs";
 import jsonata from "jsonata";
 
 export function createPlotConfig() {
-    const config = {
-    type: 'bar',
+  const config = {
+    type: "bar",
     data: {
-        labels: getNames(),
-        datasets: createDataSets()
+      labels: getNames(),
+      datasets: createDataSets()
     },
     options: {
-    plugins: {
+      plugins: {
         legend: {
-        display: false
-                }
-            },
-    indexAxis: 'y',
-    scales: {
+          display: false
+        }
+      },
+      indexAxis: "y",
+      scales: {
         xAxes: {
-            min: new Date(new Date().getFullYear(), 0, 1),
-            max: new Date(new Date().getFullYear(), 11, 31),
-            type: 'time',
-            time: {
-                unit: 'day'
-            }
+          min: new Date(new Date().getFullYear(), 0, 1),
+          max: new Date(new Date().getFullYear(), 11, 31),
+          type: "time",
+          time: {
+            unit: "day"
+          }
         },
-        yAxes:{
-        }
-            }
-        }
-    };
-    return config
+        yAxes: {}
+      }
+    }
+  };
+  return config;
 }
 
 function getNames(): string[] {
-    return getJSONData("./data/data.json", "employees.name")
+  return getJSONData("./data/data.json", "employees.name");
 }
 
 function countEmployees(): number {
-    return getJSONData("./data/data.json", `$count(employees)`)
+  return getJSONData("./data/data.json", `$count(employees)`);
 }
 
 function createDataSets() {
-    const count = countEmployees()
-    let max = 0;
-    for (let i=0; i<count; i++) {
-        const occurency = countEmployeeVaccationTimes(i)
-        if (occurency > max) {
-            max = occurency
-        }
+  const count = countEmployees();
+  let max = 0;
+  for (let i = 0; i < count; i++) {
+    const occurency = countEmployeeVaccationTimes(i);
+    if (occurency > max) {
+      max = occurency;
     }
-    const datasetArray = [];
-    for(let i=0; i<max; i++) {
-        const dataset = {
-            data: getVaccations(i),
-            backgroundColor: getColors()
-        }
-        datasetArray.push(dataset)
-    }
-    return datasetArray
+  }
+  const datasetArray = [];
+  for (let i = 0; i < max; i++) {
+    const dataset = {
+      data: getVaccations(i),
+      backgroundColor: getColors()
+    };
+    datasetArray.push(dataset);
+  }
+  return datasetArray;
 }
 
 function countEmployeeVaccationTimes(employee: number): number {
-    return getJSONData("./data/data.json", `$count(employees[${employee}].vaccation)`)
-} 
+  return getJSONData(
+    "./data/data.json",
+    `$count(employees[${employee}].vaccation)`
+  );
+}
 
 function getVaccations(position: number): string[] {
-    return getJSONData("./data/data.json", `employees.[vaccation[${position}]]`)
+  return getJSONData("./data/data.json", `employees.[vaccation[${position}]]`);
 }
 
 function getColors(): string[] {
-    return getJSONData("./data/colors.json", "colors")
+  return getJSONData("./data/colors.json", "colors");
 }
 
 // output of function is sometimes string and sometimes number, how to set output type?
-function getJSONData(jsonPath:string, jasonataExpression:string) {
-    const rawData = fs.readFileSync(jsonPath, "utf-8");
-    const data = JSON.parse(rawData);
-    const expression = jsonata(jasonataExpression)
-    return expression.evaluate(data)
+function getJSONData(jsonPath: string, jasonataExpression: string) {
+  const rawData = fs.readFileSync(jsonPath, "utf-8");
+  const data = JSON.parse(rawData);
+  const expression = jsonata(jasonataExpression);
+  return expression.evaluate(data);
 }
-
-
