ffa045d1a57a4216ac3fac0144127c7b95cf3bdb
Tobias <post@tobias-weiland.eu>
2022-06-18 15:32:46 +0200

    make employeecontroller testable, install supertest

--- src/EmployeeController.ts
+++ src/EmployeeController.ts
@@ -1,14 +1,15 @@
 import express, { Request, Response } from "express";
-import adapter = require("./EmployeeJSONAdapter");
+import { createPlotConfig } from "./EmployeeJSONAdapter";
+
+
 const app = express();
-const port = 3000;
 
 app.get("/status", (req: Request, res: Response) => {
   res.send("Server runs!");
 });
 
 app.get("/data", (req: Request, res: Response) => {
-  const config = adapter.createPlotConfig();
+  const config = createPlotConfig();
   res.send(config);
 });
 
@@ -20,6 +21,5 @@ app.get("/index.css", function (req: Request, res: Response) {
   res.sendFile("/static/index.css", { root: __dirname });
 });
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
-});
+export default app
+
