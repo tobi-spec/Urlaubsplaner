a76d242e8897b651e9b781fad352720f41c885ef
Tobias <post@tobias-weiland.eu>
2022-06-18 15:52:56 +0200

    implement first unittest for EmployeeJSONAdapter

--- __test__/EmployeeJSONAdapter.test.ts
+++ __test__/EmployeeJSONAdapter.test.ts
@@ -1,8 +1,8 @@
-import adapter = require("../src/EmployeeJSONAdapter");
+import { createPlotConfig } from "../src/EmployeeJSONAdapter";
 
 describe("Test EmployeeJSONAdapater", () => {
   test("If ", () => {
-    const result = adapter.createPlotConfig();
+    const result = createPlotConfig();
     expect(result.data.labels).toBeTruthy;
   });
 });
