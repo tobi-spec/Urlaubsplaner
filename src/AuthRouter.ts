0e4cddc9f25d0d40c29908d43aaddf160408ae92
Tobias <post@tobias-weiland.eu>
2022-07-31 22:14:18 +0200

    clean up fro auth Router functions

--- src/AuthRouter.ts
+++ src/AuthRouter.ts
@@ -8,45 +8,54 @@ const LocalStrategy = require("passport-local").Strategy
 
 const employeeJSONAdapater = new EmployeeJSONAdapater("./data/data.json")
 
-let strategy = new LocalStrategy(
+const strategy = new LocalStrategy(
   async function(username:string, password:string, done) {
     let user;
     user = await employeeJSONAdapater.getEmployeeByName(username);
-    let match = await bcrypt.compare(password, user["passwort"])
-    if (match) {
-      return done(null, user);
+    if(user) {
+      let match = await bcrypt.compare(password, user["passwort"])
+      if (match) {
+        return done(null, user);
+      } else {
+        return done(null, false, {message: 'Username or password wrong'});
+      }
     } else {
       return done(null, false, {message: 'Username or password wrong'});
     }
+
     })
 
+const sessionParams = {
+  secret: "env.secret", // move to .env file
+  resave: false,
+  saveUninitialized: true
+}
+
+const serializerFunction = (user:Express.User, done) => {
+  done(null, user);
+}
+
+const deserializerFunction = async(name:string, done) => {
+  try {
+    let user = await employeeJSONAdapater.getEmployeeByName(name);
+    if (!user) {
+      return done(new Error('user not found'));
+    }
+    done(null, user);
+  } catch (e) {
+    done(e);
+  }
+}
+
 const router = express.Router()
-router.use(session({
-    secret: "env.secret", // move to .env file
-    resave: false,
-    saveUninitialized: true
-}))
+router.use(session(sessionParams))
 router.use(express.json())
 router.use(express.urlencoded({ extended: false }));
 router.use(passport.initialize());
 router.use(passport.session());
 passport.use(strategy)
-
-passport.serializeUser(function(user:Express.User, done) {
-  done(null, user);
-});
-
-passport.deserializeUser(async (name:string, done) => {
-    try {
-      let user = await employeeJSONAdapater.getEmployeeByName(name);
-      if (!user) {
-        return done(new Error('user not found'));
-      }
-      done(null, user);
-    } catch (e) {
-      done(e);
-    }
-  });
+passport.serializeUser(serializerFunction);
+passport.deserializeUser(deserializerFunction)
   
 router.get("/login", function (req: Request, res: Response) {
   res.sendFile("./static/login.html", { root: __dirname });
