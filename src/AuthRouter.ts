7b87d99d82321dff9fbb69de45f4cd718d89376c
Tobias <post@tobias-weiland.eu>
2022-08-04 20:00:06 +0200

    implement message delivery on website

--- src/AuthRouter.ts
+++ src/AuthRouter.ts
@@ -1,33 +1,25 @@
 import express, { Request, Response } from "express";
-import session from "express-session";
+
 import passport from "passport";
 //TODO: format import?
 const initializePassport = require("./AuthService")
 
 
-const sessionParams = {
-  secret: "env.secret", // move to .env file
-  resave: false,
-  saveUninitialized: true
-}
-
 initializePassport(passport)
 
 const router = express.Router()
-router.use(session(sessionParams))
-router.use(express.json())
-router.use(express.urlencoded({ extended: false }));
 router.use(passport.initialize());
 router.use(passport.session());
 
   
 router.get("/login", function (req: Request, res: Response) {
-  res.sendFile("./static/login.html", { root: __dirname });
+  res.render("./views/login.ejs", { root: __dirname });
 });
 
 router.post("/login", passport.authenticate("local", {
   successRedirect: "/index",
   failureRedirect: "/login?error=true",
+  failureFlash: true
 }))
 
 module.exports = router;
--- src/EmployeeController.ts
+++ src/EmployeeController.ts
@@ -1,11 +1,23 @@
 import express, { Request, Response } from "express";
 import flash from "express-flash";
+import session from "express-session";
 import { PlotConfig } from "./models/PlotConfig";
 //TODO: format import?
 const authRouter = require("./AuthRouter")
 
+const sessionParams = {
+  secret: "env.secret", // move to .env file
+  resave: false,
+  saveUninitialized: true
+}
 
 const app = express();
+app.set("view-engine", "ejs")
+app.set('views', __dirname)
+app.use(session(sessionParams))
+app.use(flash())
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }));
 
 app.get("/status", (req: Request, res: Response) => {
   res.json("Server runs!");
@@ -17,11 +29,11 @@ app.get("/data", (req: Request, res: Response) => {
 });
 
 app.get("/index", function (req: Request, res: Response) {
-  res.sendFile("./static/index.html", { root: __dirname });
+  res.render("./views/index.ejs", { root: __dirname });
 });
 
 app.get("/index.css", function (req: Request, res: Response) {
-  res.sendFile("/static/index.css", { root: __dirname });
+  res.sendFile("/views/index.css", { root: __dirname });
 });
 
 app.use(authRouter)
