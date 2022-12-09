ac5d7e566ea149b746f04f7bdb587fc0b5359ecf
Tobias <post@tobias-weiland.eu>
2022-12-09 20:57:38 +0100

    move cypress to testfolder

--- cypress.config.ts
+++ cypress.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from "cypress";
 export default defineConfig({
   e2e: {
     baseUrl: 'http://localhost:3000',
-    specPattern: "cypress/e2e/**/*.cy.{js,jsx,ts,tsx}",
+    specPattern: "test/e2e/**/*.cy.{js,jsx,ts,tsx}",
     supportFile: false
     },
   },
