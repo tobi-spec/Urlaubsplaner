3e92c45c645fbb0ab38834e4dca63a10bb945a0a
Tobias <post@tobias-weiland.eu>
2022-12-10 09:29:52 +0100

    clean up EmployeeController

--- src/EmployeeController.ts
+++ src/EmployeeController.ts
@@ -1,12 +1,10 @@
 import express, { Request, Response } from "express";
 import flash from "express-flash";
 import session from "express-session";
-
 import {
   strategy,
   serializerFunction,
   deserializerFunction,
-  isAuth
 } from "./authentification/AuthService";
 import passport from "passport";
 //TODO: format import?
@@ -23,19 +21,15 @@ const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-
 app.set("view-engine", "ejs");
 app.set("views", __dirname);
-
 app.use(session(sessionParams));
 app.use(flash());
-
 passport.use(strategy);
 passport.serializeUser(serializerFunction);
 passport.deserializeUser(deserializerFunction);
 app.use(passport.initialize());
 app.use(passport.session());
-
 app.use(authRouter);
 app.use(calendarRouter)
 
@@ -46,6 +40,5 @@ app.get("/status", (req: Request, res: Response) => {
 app.get("/", (req: Request, res: Response) => {
   res.redirect("/login");
 });
-
 
 export default app;
