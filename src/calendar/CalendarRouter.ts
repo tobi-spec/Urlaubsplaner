d5ab432ec937e59345d84a945914963431ddf907
tobi-spec <post@tobias-hahn.eu>
2022-12-13 19:33:30 +0100

    add routes to CalendarRouter

--- src/calendar/CalendarRouter.ts
+++ src/calendar/CalendarRouter.ts
@@ -1,8 +1,11 @@
 import express, { NextFunction, Request, Response } from "express";
 import { CalendarConfig } from "./CalendarConfig";
 import { isAuth } from "../authentification/AuthService";
+import { CalendarService } from "./CalendarService";
 
 const calendarRouter = express.Router();
+
+const calendarService = new CalendarService("./data/data.json")
 
 calendarRouter.get("/data", isAuth, (req: Request, res: Response) => {
     const config = new CalendarConfig();
@@ -19,6 +22,21 @@ res.sendFile("/views/calendar.css", { root: __dirname + '/../' });
 
 calendarRouter.get("/icons8-logout-50.png", function (req: Request, res: Response) {
 res.sendFile("/views/icons8-logout.png", { root: __dirname + '/../' });
+});
+
+calendarRouter.get("/items", (req: Request, res: Response) => {
+  const items = calendarService.createItems();
+  res.json(items);
+});
+
+calendarRouter.get("/groups", async (req: Request, res: Response) => {
+const groups = await calendarService.createGroups();
+res.json(groups);
+});
+
+calendarRouter.get("/options", isAuth, (req: Request, res: Response) => {
+const options = calendarService.createOptions();
+res.json(options);
 });
 
 export default calendarRouter
