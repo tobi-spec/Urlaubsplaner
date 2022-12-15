d799ae48a8cd042c53793190af952d46b7aba1f5
tobi-spec <post@tobias-hahn.eu>
2022-12-15 22:57:36 +0100

    create classes for unittests

---
+++ test/unittests/CalendarService.test.ts
@@ -0,0 +1,12 @@
+import { JsonAdapater } from "../../src/JsonAdapter";
+import { expect } from "@jest/globals";
+
+describe("unittests for CalendarService", () => {
+
+  test("test createItems()", async () => {});
+
+  test("test createGroups()", async () => {});
+
+  test("test createOptions()", async () => {});
+
+  });
--- test/unittests/JsonAdapter.test.ts
+++ test/unittests/JsonAdapter.test.ts
@@ -1,22 +1,10 @@
 import { JsonAdapater } from "../../src/JsonAdapter";
-import fs from "fs";
-import bcrypt from "bcrypt";
 import { expect } from "@jest/globals";
 
-describe("unittests for EmployeeJSONAdapater", () => {
-  const testPath = "./data/testData.json";
+describe("unittests for JsonAdapater", () => {
 
-  test("test getEmployeeByName()", async () => {
-    const employeeJSONAdapater = new JsonAdapater(testPath);
-    const employee = await employeeJSONAdapater.getEmployeeByName("employee1");
+  test("test getJsonData()", async () => {});
 
-    expect(Object.values(employee)[0]).toBe("employee1");
-    expect(Object.values(employee)[1]).toBe(
-      "$2a$10$I8nZ18bhG7RfaZl1g8tMOOhrpT/hDSoo9Of.3gzUmrznXKJftUU2a"
-    );
-    expect(Object.values(employee)[2]).toStrictEqual([
-      ["2022-01-01", "2022-03-03"],
-      ["2022-10-01", "2022-11-01"]
-    ]);
+  test("test getJSONDataByExpression()", async () => {});
+
   });
-});
