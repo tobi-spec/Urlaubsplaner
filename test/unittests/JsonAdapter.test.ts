7d988f31910d30ebfcfb7ab0c5e5b4d3bee1a29b
tobi-spec <post@tobias-hahn.eu>
2022-12-17 17:09:57 +0100

    implement tests for JsonAdapater, move testfiles in subfolder

--- test/unittests/JsonAdapter.test.ts
+++ test/unittests/JsonAdapter.test.ts
@@ -3,8 +3,13 @@ import { expect } from "@jest/globals";
 
 describe("unittests for JsonAdapater", () => {
 
-  test("test getJsonData()", async () => {});
+  test("test getJsonData()", async () => {
+    const result = new JsonAdapater("./test/testdata/TestHolidays.json").getJsonData()
+    expect(result[0]).toStrictEqual({"end": "2022-03-03", "group": "1", "id":1, "start": "2022-01-01"})
+  });
 
-  test("test getJSONDataByExpression()", async () => {});
-
+  test("test getJSONDataByExpression()", async () => {
+    const result = new JsonAdapater("./test/testdata/testEmployees.json").getJSONDataByExpression("employees")
+    expect(result[0]).toStrictEqual({"id": "1", "name": "employee1", "password": "$2a$10$I8nZ18bhG7RfaZl1g8tMOOhrpT/hDSoo9Of.3gzUmrznXKJftUU2a"})
+  });
   });
